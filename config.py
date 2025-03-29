90d4e23234cdaf610ba2eb6cf74207ead9c50233
dgordover@gmail.com <dgordover@gmail.com>
2025-03-29 16:36:31 +0300

    now there are secretsss

--- config.py
+++ config.py
@@ -1,12 +1,18 @@
 from sqlalchemy.engine import URL
+import os
+from dotenv import load_dotenv
 
+load_dotenv()  # Load environment variables from .env
+db_user = os.getenv("AWS_DB_USER")
+db_password = os.getenv("AWS_DB_PASSWORD")
+db_endpoint = os.getenv("AWS_DB_ENDPOINT")
 
 class Config:
     SQLALCHEMY_DATABASE_URI = URL.create(
         "mssql+pyodbc",
-        username="admin",
-        password="chillie&sammyrock",
-        host="woof-together-db.cteosyo8km48.us-east-1.rds.amazonaws.com",
+        username=db_user,
+        password=db_password,
+        host=db_endpoint,
         port=1433,
         database="WoofTogether",
         query={
