f8e6972ed2dc60f6f4ed8d4d770fb47988bc0fdb
Amit Neman (Technology) <Amit.Neman@keshet.dom>
2025-08-02 12:26:44 +0300

    new cognito

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,13 @@
 export const environment = {
     production: true,
-    cognitoDomain: 'https://us-east-1v1s8kqzxk.auth.us-east-1.amazoncognito.com',
-    clientId: '5r2o2aopa92hgot078tfhvkf41',
-    clientSecret:'37qqnss0umi5sq1s02ldf74spkjje2k9t8k3u1siagerjr9cn19',
+    cognitoDomain: 'https://us-east-19tpnuqjde.auth.us-east-1.amazoncognito.com',
+    clientId: '4kg1ackgcuha9i3oem4crqt1s6',
+    clientSecret:'og0v1n7916l6hsu5lt5s29377ua75ebmcjonsihnblqs0vqprdh',
     // redirectUri:'https://ui.dhz62hiqax4cs.amplifyapp.com/',
     redirectUri:'http://localhost:4200/',
     grantType: 'authorization_code',
-    userPoolId:'us-east-1_V1S8KqZxK',
+    userPoolId:'us-east-1_9tpNUqjdE',
     region:'us-east-1',
     apiUrl: 'https://nt8exuq2h9.execute-api.us-east-1.amazonaws.com/dev',
-      loginUrl: 'https://us-east-1v1s8kqzxk.auth.us-east-1.amazoncognito.com/login?client_id=5r2o2aopa92hgot078tfhvkf41&response_type=code&scope=email+openid+profile&redirect_uri=http://localhost:4200/'
+      loginUrl: 'https://us-east-19tpnuqjde.auth.us-east-1.amazoncognito.com/login?client_id=4kg1ackgcuha9i3oem4crqt1s6&response_type=code&scope=email+openid+profile&redirect_uri=http://localhost:4200/'
   };
